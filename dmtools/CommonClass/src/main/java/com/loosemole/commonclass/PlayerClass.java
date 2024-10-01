e6121bd6fb51c904c3a12335ce7d386c9f121d9e
looseMole <62012079+looseMole@users.noreply.github.com>
2024-10-01 14:24:34 +0200

    PlayerClass getters and setters

--- dmtools/CommonClass/src/main/java/com/loosemole/commonclass/PlayerClass.java
+++ dmtools/CommonClass/src/main/java/com/loosemole/commonclass/PlayerClass.java
@@ -1,6 +1,7 @@
 package com.loosemole.commonclass;
 
 import java.util.ArrayList;
+import java.util.List;
 
 public class PlayerClass {
     private String name;
@@ -10,5 +11,29 @@ public class PlayerClass {
     public PlayerClass(String name) {  // Placeholder constructor, only assigning name.
         this.name = name;
         // TODO make proper constructors for PlayerClass
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDesc() {
+        return desc;
+    }
+
+    public void setDesc(String desc) {
+        this.desc = desc;
+    }
+
+    public List<ClassLevel> getLevels() {
+        return levels;
+    }
+
+    public void setLevels(ArrayList<ClassLevel> levels) {
+        this.levels = levels;
     }
 }
